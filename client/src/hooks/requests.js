0d55766fb4aee9fb31ee7eec1bdcd41872f75aa4
ChristopherL <anbchris@gmail.com>
2026-01-28 12:17:05 -0500

    create server and add backend endpoint fetch to httpGetPlanets as per request in requests.js client code

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,10 @@
+const API_URL = "http://localhost:8000";
+
 async function httpGetPlanets() {
   // TODO: Once API is ready.
   // Load planets and return as JSON.
+  const planets = await fetch(`${API_URL}/planets`);
+  return await planets.json();
 }
 
 async function httpGetLaunches() {
@@ -18,9 +22,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
